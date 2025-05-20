504a4e7c5683bbec84fa4846ce26d00bb971da9d
VANSHIKA <vanshikaavailable@gmail.com>
2025-05-20 21:33:28 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/student_marks_csv/',  // <--- set this to your repo name
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+
 });
